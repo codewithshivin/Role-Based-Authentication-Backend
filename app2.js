89accf93b4088785bd20bedb7756827ceafe320d
codewithshivin <codewithshivin@gmail.com>
2026-05-13 14:34:13 +0530

    Update app2.js

--- app2.js
+++ app2.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
+const authRoutes = require("./routes/routes");
+const musicRoutes = require("./routes/music.routes");
+
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/music", musicRoutes);
+
 
 module.export = app;
