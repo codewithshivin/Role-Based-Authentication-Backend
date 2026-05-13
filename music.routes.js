abdea0af8fcc424858b2718ab319f19b23a60172
codewithshivin <codewithshivin@gmail.com>
2026-05-13 14:41:45 +0530

    Add files via upload

---
+++ music.routes.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const musicController = require("../controolers/music.controoler");
+const authMiddlewarec = require("../middleware/auth.middleware")
+const multer = require("multer");
+
+const upload = multer({
+    storage: multer.memoryStorage()
+})
+
+const router = express.Router();
+
+
+router.post("/upload", authMiddleware.authArtist, upload.single("music"), musicController.createMusic)
+
+router.post("/album", authMiddleware.authArtist, musicController.createAlbum)
+
+
+router.get("/", authMiddleware.authUser, musicController.getAllMusics)
+router.get("/albums", authMiddleware.authUser, musicController.getAllAlbums)
+
+router.get("/albums/:albumId", authMiddleware.authUser, musicController.getAlbumById)
+
+
+
+module.exports = router;
